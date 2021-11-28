a026b0caca74085b72632caede27a574f076530b
Mátyás Turcsányi <turcsanyi.matyas@gmail.com>
2021-11-28 11:21:21 +0100

    add heroku postbuild to package.json corr3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/dist/ang-heroku'));
+app.use(express.static(__dirname+'/dist/my-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname+'dist/ang-heroku/index.html')),
+    res.sendFile(path.join(__dirname+'dist/my-app/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
