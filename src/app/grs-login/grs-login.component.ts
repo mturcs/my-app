48c77b04c61956ab55b9ad28d6114fc0301576ab
Mátyás Turcsányi <turcsanyi.matyas@gmail.com>
2021-12-02 18:27:32 +0100

    change restapi url

--- src/app/grs-login/grs-login.component.ts
+++ src/app/grs-login/grs-login.component.ts
@@ -136,6 +136,8 @@ export class GrsLoginComponent implements OnInit {
 
   createLoginRec(loggedInRec: Array<loggedSession>) {
 
+
+    //this.http.post<object>('http://localhost:3000/app/create/loginsession', this.loggedInRec, httpOptions).subscribe()
     //this.http.post<object>('http://localhost:80/app/create/loginsession', this.loggedInRec, httpOptions).subscribe()
     //this.http.post<object>('/app/create/loginsession', this.loggedInRec, httpOptions).subscribe()
 
@@ -151,7 +153,7 @@ export class GrsLoginComponent implements OnInit {
       setTimeout(() => {
         let loginName = " "
         this.http.get<[registrySch]>
-          ('https://my-appserver.herokuapp.com:80/app/qemail/' + mQuerystring).subscribe(function (data) {
+          ('http://mturcs-my-app.herokuapp.com/app/qemail/' + mQuerystring).subscribe(function (data) {
             //this.loginName = data[0].first_name + " " + data[0].last_name
             resolve(data)
             reject("DB unreachable")
